9f111d5e8e9506a3d8c0836ee812a453139ca5d7
ArpitAware <111407387+ArpitAware@users.noreply.github.com>
2025-03-14 16:08:28 +0530

    update

--- server/index.js
+++ server/index.js
@@ -21,9 +21,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: ['https://digital-keys-marketplace.onrender.com', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
