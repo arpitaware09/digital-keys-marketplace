d3f8cc50095ddd8e0f2e2ac7180e587e77a18538
ArpitAware <111407387+ArpitAware@users.noreply.github.com>
2025-03-14 16:02:08 +0530

    update

---
+++ client/public/config.js
@@ -0,0 +1,5 @@
+window.ENV = {
+  REACT_APP_API_URL: "https://digital-keys-marketplace-server.onrender.com/api",
+  REACT_APP_RAZORPAY_KEY_ID: "rzp_test_BnFhgjViX6iODO",
+  REACT_APP_CLOUDINARY_CLOUD_NAME: "dkdpntjxe"
+}; 
